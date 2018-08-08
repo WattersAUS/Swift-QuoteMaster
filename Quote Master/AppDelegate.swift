f22178eace04509fccdb7651467cf2e43f7a2c51
Graham Watson <grahamj.watson@gmail.com>
2018-08-08 19:24:52 +0100

    Initial Project Checkin
    Initial XCode Project files

---
+++ Quote Master/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Quote Master
+//
+//  Created by Graham Watson on 08/08/2018.
+//  Copyright © 2018 Graham Watson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Quote Master/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Quote Master
+//
+//  Created by Graham Watson on 08/08/2018.
+//  Copyright © 2018 Graham Watson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
